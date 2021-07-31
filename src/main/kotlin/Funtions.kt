43fc26c7d33a2877d1670c81f2b94c62dcd1015f
RNasserB <rnbprojetos@gmail.com>
2021-07-30 22:25:38 -0300

    Add fun maiorNumero

--- src/main/kotlin/Funtions.kt
+++ src/main/kotlin/Funtions.kt
@@ -1,7 +1,16 @@
 fun main (){
-    print(soma(15, 5))
+    print(maiorNumero(11, 12, 10))
 }
 
 fun soma(numeroUm: Int, numeroDois: Int): Int {
     return numeroUm + numeroDois
+}
+
+fun maiorNumero(numeroUm: Int, numeroDois: Int, numeroTres: Int): Int {
+    if ((numeroUm >= numeroDois) and (numeroUm >= numeroTres)){
+        return numeroUm
+    } else if (numeroDois >= numeroTres){
+        return numeroDois
+    } else return numeroTres
+
 }
