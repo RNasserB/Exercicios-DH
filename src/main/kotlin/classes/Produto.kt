758b18f880420c63b4f9cca22df350d4a43bbb08
RNasserB <rnbprojetos@gmail.com>
2021-08-03 22:36:36 -0300

    Classes Apos e Prod Revised

--- src/main/kotlin/classes/Produto.kt
+++ src/main/kotlin/classes/Produto.kt
@@ -4,7 +4,7 @@ package classes
 class Produto(var listaNumeros: List<Int>){
 
     fun produtoFinal(): Int{
-        var resultado: Int = 1
+        var resultado = 1
         for (num in listaNumeros){
             resultado *= num
         }
