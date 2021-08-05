5a9963b84772c92790f9199e166081adf89cf103
RNasserB <rnbprojetos@gmail.com>
2021-08-05 00:25:09 -0300

    Correção Energia Jogadores Treinamento

--- src/main/kotlin/classes/JogadoresTreinamento/JogadorDeFutebol.kt
+++ src/main/kotlin/classes/JogadoresTreinamento/JogadorDeFutebol.kt
@@ -2,16 +2,24 @@ package classes.JogadoresTreinamento
 
 class JogadorDeFutebol(val nome: String, var energia: Int, var alegria: Int, var gols: Int, var experiencia: Int) {
 
-    fun fazerGol(){
-        energia -= 5
-        alegria += 10
-        gols += 1
-        println("GOOOOL!")
-    }
+    fun fazerGol() {
+        if (energia <= 5) {
+            energia -= 5
+            alegria += 10
+            gols += 1
+            println("GOOOOL!")
+        } else {
+            println("Sem Energia")
+        }
+        }
 
-    fun correr(){
-        energia -= 10
-        println("Cansei")
-    }
+        fun correr() {
+            if (energia <= 10) {
+                energia -= 10
+                println("Cansei")
+            } else {
+                println("Sem Energia")
+            }
+        }
 
-}
+    }
