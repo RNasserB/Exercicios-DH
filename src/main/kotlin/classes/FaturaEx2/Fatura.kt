d0ef9b26a1513063f816d5c5dc5f5d561e792ec8
RNasserB <rnbprojetos@gmail.com>
2021-08-09 22:21:36 -0300

    Exercicio 2 Corrigido

--- src/main/kotlin/classes/FaturaEx2/Fatura.kt
+++ src/main/kotlin/classes/FaturaEx2/Fatura.kt
@@ -6,7 +6,6 @@ class Fatura(var listaItens: ArrayList<Item>) {
     fun totalDaFatura(): Double {
         var resultado = 0.0
 
-        listaItens.forEach() { resultado = ((resultado + it.quantidadeComprada) * it.preçoUnitario) }
         listaItens.forEach() { resultado += (it.quantidadeComprada * it.preçoUnitario) }
         return resultado
 
--- src/main/kotlin/classes/FaturaEx2/RunFatura.kt
+++ src/main/kotlin/classes/FaturaEx2/RunFatura.kt
@@ -2,7 +2,7 @@ package classes.FaturaEx2
 
 fun main() {
 
-    val itemComputador = Item(10, "Computador", 2, 5.0)
+    val itemComputador = Item(10, "Computador", 3, 5.0)
     val itemMonitor = Item(15, "Monitor", 3, 10.0)
     val itemTeclado = Item(15, "Monitor", -5, -5.0)
 
